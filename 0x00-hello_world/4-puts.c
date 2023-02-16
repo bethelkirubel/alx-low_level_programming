71fc104d522ae94e98715d9a28fc11876d773667
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-02-16 15:33:16 -0800

    hoho

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
 #include <studio.h>
+
 /**
-*   main - Entry point 
+*main - Entry point 
 *                  
-*   Return Always: 0 (Success)
+*Return Always: 0 (Success)
 *
 */
+
 int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle\n");
+
 return (0);
+
 } 
