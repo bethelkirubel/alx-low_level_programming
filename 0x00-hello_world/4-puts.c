9c6f9f0d37be7b2449f703a6873627b722ec2994
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-02-16 15:01:05 -0800

    kkohkg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,30 @@
 #!/bin/bash
-puts ("\"Programming is like building a multilingual puzzle);
+#include <studio.h>
+
+
+/*
+ *File: 4-puts.c
+ * Author: Bethel Kirubel <bethelki@gmail.com>
+ *
+ */
+
+#include <studio.h>
+
+/**
+*   printing a  sentence 
+*                  
+*   return Always: 0.
+*
+*/
+
+int main(void)
+
+
+{
+
+puts ("\"Programming is like building a multilingual puzzle\n);
+ 
+return(0);
+
+
+} 
