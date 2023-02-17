5bf73c5f74f628e896051061649f328ff5f46322
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-02-17 01:51:01 -0800

    finally

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * use printf
- *  
- * Return: Always 0 (Success)
+ * main - use  printf
  *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
- {
-    printf("with proper grammar, but the outcome is a piece of art,);
+{
+printf("with proper grammar, but the outcome is a piece of art,);
 
-    return (0);
- } 
+return (0);
+}
