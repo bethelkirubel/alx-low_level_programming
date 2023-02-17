de272459ae0b1f3e7aa59874803f88b21784830e
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-02-17 07:07:19 -0800

    lolololo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  *  main - print without printf or puts command
@@ -6,9 +8,11 @@
  *  Return: Always 1 (Success)
  *
  */
+
 int main(void)
 {
-write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19, 59);
+write(STDOUT_FILENO, "and that piece of art is useful\"
+-Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
