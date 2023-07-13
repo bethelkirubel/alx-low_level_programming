f50a15a9a50421cf4290decb58342bf65fe302c9
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-13 11:45:00 -0700

    prontf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Description: a program that prints "with proper grammar, but the outcome is a piece of art," and returns 0
+ * Description: "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
-int main(void);
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
