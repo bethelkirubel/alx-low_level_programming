0e7deeadb909bbc013b513d41c4db5654ab446bc
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-13 11:41:34 -0700

    yay

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,8 +9,10 @@
  */
 
 
-int main(void);
+int main(void)
+
 {
-	puts("\""Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
