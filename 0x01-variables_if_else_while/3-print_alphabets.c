84949a9325c9a1075f8e503f4862cbaa3aa3c70f
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-15 12:30:07 -0700

    betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,9 @@ int main(void)
 {
 	char c;
 
-	for (c = 'A' ; c <= 'Z'; c++)
+	for (c = 'a' ; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 	putchar ('\n');
 		return (0);
