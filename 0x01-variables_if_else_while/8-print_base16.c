258b65e5088cfbc8c551f896e75a77f41c0fb48a
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-16 04:05:20 -0700

    num

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	char c;
+int num;
+
+	for (num = '0' ; c <= '9'; c++)
+		putchar(num);
+	for (c = 'a' ; c <= 'f'; c++)
+		putchar(c);
+	putchar ('\n');
+		return (0);
+}
