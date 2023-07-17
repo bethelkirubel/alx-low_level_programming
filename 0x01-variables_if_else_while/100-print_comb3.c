b6b04bf3edac959982544db5fad7c6fa5e942067
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-17 09:09:59 -0700

    yuppy

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,20 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+	for (digit1 = '0'; digit1 < '9'; digit1++)
 
-			if (digit1 == 8 && digit2 == 9)
+	{
+		for (digit2 = '1'; digit2 <= '9'; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+
+			if (digit2 <= digit1)
 				continue;
 			putchar(',');
 			putchar(' ');
 		}
-	}
-
+}
 	putchar('\n');
-
 	return (0);
 }
