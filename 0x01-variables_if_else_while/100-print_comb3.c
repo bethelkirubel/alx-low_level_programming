7ca651eae96e508a7c54473a13c374194cd2dfd3
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-17 08:08:23 -0700

    kite

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,14 +16,10 @@ for (num1 = '0' ; num1 <= '8'; num1++)
 {
 		putchar(num1);
 		putchar(num2);
-
-	if ((num1 = num2) || (num1 > num2))
-	{
+putchar(',');
+putchar(' ');
+	while ((num1 = num2) || (num1 > num2))
 		continue;
-	}
-
-	putchar(',');
-	putchar(' ');
 	}
 }
 putchar('\n');
