d9be0a7aa537a2728dac4bcc52d1e0192e65169f
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-17 08:12:40 -0700

    hey there

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,25 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int num1, num2;
-for (num1 = '0' ; num1 <= '8'; num1++)
-{
-	for (num2 = '1' ; num2 <= '9' ; num2++)
-{
-		putchar(num1);
-		putchar(num2);
-putchar(',');
-putchar(' ');
-	while ((num1 = num2) || (num1 > num2))
-		continue;
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
+
+	putchar('\n');
+
+	return (0);
 }
