256c693da621a3ed9e69b3bed8bcf3e9550a981a
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-19 16:39:12 -0700

    alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char letter;
+
+	int i = 0;
+
+	while (i <= 10)
+
+		for (letter = 'a' ; letter <= 'z' ; letter++)
+				{
+				_putchar(letter);
+				}
+
+				_putchar('\n');
+}
