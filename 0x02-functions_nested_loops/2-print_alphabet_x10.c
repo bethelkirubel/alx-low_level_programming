b4c949836029e142694f5a42334b944943c14d47
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-20 03:35:31 -0700

    finally

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,12 @@ void print_alphabet_x10(void)
 
 {
 	char letter;
-
 	int i = 0;
 
-while (i <= 9)
-{
-for (letter = 'a' ; letter <= 'z' ; letter++)
-{
-				_putchar(letter);
-				}
-				_putchar('\n');
-				i++;
-}
+	while (i++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
