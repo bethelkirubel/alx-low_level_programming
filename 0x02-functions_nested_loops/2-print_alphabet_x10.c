46563906d8e4e818e4e6058aa18cb70c08f58d1d
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-19 17:08:48 -0700

    warning

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	int i = 0;
 
-while (i <= 10)
+while (i <= 9)
 {
 for (letter = 'a' ; letter <= 'z' ; letter++)
 {
@@ -19,5 +19,5 @@ for (letter = 'a' ; letter <= 'z' ; letter++)
 				}
 				_putchar('\n');
 				i++;
-				}
+}
 }
