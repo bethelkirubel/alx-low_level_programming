a716d120db17cd2c1d73ede440249bfe458c2619
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-22 15:20:26 -0700

    ha

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * print_to_98 - print natural number between any passed integer and 98
  * @n: intiger input
  */
@@ -9,30 +9,36 @@ void print_to_98(int n)
 {
 int i, j;
 
+if (n <= 98)
+{
 for (i = n; i <= 98; i++)
 {
-if (n != 98)
+if (n == 98)
 {
 _putchar(n);
+}
+else if (n != 98)
+{
+_putchar (n);
 _putchar(',');
 _putchar(' ');
 }
-if(( n = 98) )
+}
+}
+else if (n > 98)
 {
-_putchar(n);
-}
-}
 for (j = n; j >= 98; j++)
 {
-if (n > 98)
+if (n != 98)
 {
 _putchar (n);
 _putchar (',');
 _putchar (' ');
 }
-if( (n = 98) ) 
+else if (n == 98)
 {
 _putchar (n);
+}
 }
 }
 _putchar('\n');
