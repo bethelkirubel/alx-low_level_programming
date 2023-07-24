9cc2f78c9e348602f03fd9b497527198e2b897cf
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-23 19:09:18 -0700

    fizzbuzz by betty

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ *main - Prints a Fizz Buzz program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i <= 9)
+{
+putchar(i + '0');
+}
+else if (i > 9)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+}
+putchar(' ');
+}
+return (0);
+}
