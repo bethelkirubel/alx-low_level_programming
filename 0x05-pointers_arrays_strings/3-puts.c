2be59bb4dc7a8bf0b89b22fdc087b986380a33d2
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-25 03:06:14 -0700

    *str++

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Display a string
+ * @str: String to show
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+
+{
+while (*str != '\0')
+{
+putchar(*str++);
+}
+putchar('\n');
+}
