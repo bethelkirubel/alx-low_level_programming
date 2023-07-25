7509a89224aeb505f21897995f52bd4eca58d11a
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-25 02:45:00 -0700

    by using strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculate the lenght of a string
+ * @s: string
+ *
+ * Return: lenght of the s string
+ */
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
