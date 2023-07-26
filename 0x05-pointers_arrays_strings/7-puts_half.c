1368a43e166d1ede5d14dc1348ccba0fdada2a2d
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-25 17:31:58 -0700

    fixed

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half -print half of the string
+ * @str: string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int len, n, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (n = (len - 1) / 2; n < len ; n++)
+		{
+			putchar(str[n]);
+		}
+	}
+	putchar('\n');
+}
