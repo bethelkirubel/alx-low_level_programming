0c8f6d9405fb81caf863d7fb739e4ccc11e0d457
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-07-30 15:41:04 -0700

    betty trouble

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,16 +11,17 @@
 
 void print_number(int n)
 {
-	unsigned int num;
-	num = n;
-	if (n < 0)
-	{
-		_putchar('-');
-	        num = -n;
-	}
-	if (num / 10)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + '0');
+unsigned int num;
+num = n;
+
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+if (num / 10)
+{
+print_number(num / 10);
+}
+_putchar((num % 10) + '0');
 }
