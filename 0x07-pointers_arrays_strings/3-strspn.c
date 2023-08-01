e9cd07b10c62053e439143cc02d87599db4fb133
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-01 16:32:08 -0700

    c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,16 +10,21 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
-int count = 1;
+unsigned int count = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
+{
 count++;
-else if (s[i] != accept[j])
 break;
+}
+else if (s[i] != accept[j])
+{
+break;
+}
 }
 }
 return (count);
