af2919a7c376a51a1ab232cc92d564c5b30ac226
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-01 11:29:58 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies elements of another array to a given array
+ * @dest: array output and to be added upon
+ * @src: array input and added
+ * @n: bytes of memory
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
