0d4339db91b7ebf53ce2287be12b26f7e80d2395
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-09 12:23:20 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,3 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +20,3 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif 
