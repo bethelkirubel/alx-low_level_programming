3c48ca053079aef557c3b087b7949986fca4b7e6
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-09 03:59:47 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string and can use both malloc and free
+ * @str: pointer to string
+ * Return: string or NULL
+ */
+
+char *_strdup(char *str)
+{
+char *str2;
+unsigned int i = 0;
+unsigned int j = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (str[i])
+i++;
+
+str2 = malloc(sizeof(char) * i);
+
+if (str2 == NULL)
+return (NULL);
+
+while (str[j])
+{
+str2[j] = str[j];
+j++;
+}
+return (str2);
+}
