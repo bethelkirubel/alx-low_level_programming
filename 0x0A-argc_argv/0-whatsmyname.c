6660cf44767c3b94ccca97ead093669ab1d730a0
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-09 14:36:28 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  prints the program name
+ * @argc: counts argument
+ * @argv: lists arguments
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+void(argc);
+
+printf("%s", argv[0]);
+return (0);
+}
