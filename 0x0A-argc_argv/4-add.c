a704ab8dde4b0733398171b5c2f0bbd713b75cbc
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-12 10:58:50 -0700

    added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,10 +18,10 @@ for (i = 1; i < argc; i++)
 for (j = 0; argv[i][j] != '\0'; j++)
 {
 if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
-			{
-				printf("Error\n");
-				return (1);
-			}
+{
+printf("Error\n");
+return (1);
+}
 }
 sum += atoi(argv[i]);
 }
