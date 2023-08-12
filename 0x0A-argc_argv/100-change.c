9d67f1f66fc0bd19a25a2475abf5dac8b158053f
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-12 13:59:33 -0700

    good

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints out  changes
+ * @argc: count arguments
+ * @argv: list argument value
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+int cent, ncent = 0;
+
+if (argc == 1 || argc > 2)
+{
+printf("ERROR\n");
+return (1);
+}
+cent = atoi(argv[1]);
+if (cent < 0)
+{
+printf("0\n");
+}
+
+while (cent > 0)
+{
+if (cent >= 25)
+cent -= 25;
+else if (cent >= 10)
+cent -= 10;
+else if (cent >= 5)
+cent -= 5;
+else if (cent >= 2)
+cent -= 2;
+else if (cent >= 1)
+cent -= 1;
+
+ncent += 1;
+}
+printf("%d\n", ncent);
+return (0);
+}
