e38e2c5f8b0e998ed05ef18ff03db4a743a25bcb
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-12 07:40:09 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds number arguments
+ * @argc: counts all arguments
+ * @argv: lists all arguments
+ * Return: sum or error
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j;
+int sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("ERROR\n");
+return (1);
+}
+sum += atoi(argv[i]);
+}
+}
+printf("%d\n", sum);
+return (0);
+}
