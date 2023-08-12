3a370b606c7cd6b61f33b3b2dfe2a2db5e0d07da
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-12 12:48:48 -0700

    cmon

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - multiplies two numbers
@@ -15,14 +14,12 @@ int num1, num2,  mul = 0;
 
 if (argc != 3)
 {
-
 printf("ERROR\n");
 return (1);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-
 mul = num1 *num2;
 
 printf("%d\n", mul);
