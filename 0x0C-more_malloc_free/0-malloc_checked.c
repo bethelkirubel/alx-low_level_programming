bf1f404fe77639121aa2c243f2104df153618a11
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-13 10:27:18 -0700

    malloc pointer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function should cause normal process termination with a status value of 98
+ * @argc: counts arguments
+ * @argv: lists argument values
+ * Return: pointer of malloc
+ */
+void *malloc_checked(unsigned int b)
+{
+ptr = *b;
+b = malloc(unsigned int, * 1);
+
+return (*b);
+}
