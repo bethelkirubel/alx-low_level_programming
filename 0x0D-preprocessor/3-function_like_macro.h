ea13be8017ad5d76e1af98bc053fb746c204a164
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-15 11:00:02 -0700

    commented

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,11 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
+
+/**
+ * 3-function_like_macro.h - file that defines ABS to find the absolute value
+ * @x: input
+ */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
