17f4a72363fa20afdc10e9efaac3e063ebb8e9ad
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-18 10:54:47 -0700

    march

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 /**
  * sum_them_all - sum the given numbers
  * @n: number of all arguments
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIDIAC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
@@ -8,4 +14,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
-
+#endif
