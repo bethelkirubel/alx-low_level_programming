eb3a9937da1c5026b8837fb159e2dd0f7748ecf6
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-24 09:37:06 -0700

    easy one

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the list along with its size
+ * @h:head node in singly linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t num_nod = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[%d] %s\n", 0, "(nil)");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+num_nod++;
+h = h->next;
+}
+return (num_nod);
+}
