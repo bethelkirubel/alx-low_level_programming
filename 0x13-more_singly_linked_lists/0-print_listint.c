3fc895e974b1d57c7c93bebacbbc0a748a2d06db
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-28 11:50:49 -0700

    now

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,13 @@
 size_t print_listint(const listint_t *h)
 {
 size_t i;
-if (h == NULL)
-return (NULL);
+while (h != NULL)
+{
 for (i = 0; h; i++)
 {
 printf("%d\n", h->n);
 h = h->next;
+}
 }
 return (i);
 }
