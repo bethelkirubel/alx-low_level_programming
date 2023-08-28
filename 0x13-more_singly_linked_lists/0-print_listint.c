ac458d553ff8efaba790ab00199182b1f6ecd34a
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-08-28 11:34:38 -0700

    progress

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_listint - prints all elements in listint_t lists
+ * @h: string where we list it
+ * Return: number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+size_t i;
+
+for (i = 0; h; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
+}
