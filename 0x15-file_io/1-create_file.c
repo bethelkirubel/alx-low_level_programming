13573c4db60b576f04b8ca81c810aa925f8d54a9
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-09-04 12:37:59 -0700

    bet

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file
+ * @filename: the filename
+ * @text_content: content of the file
+ * Return: NULL or 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, i;
+int r;
+
+if (!filename)
+return (-1);
+
+if (!text_content)
+text_content = "";
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+
+if (fd == -1)
+return (-1);
+
+for (i = 0; filename[i]; i++)
+;
+r = write(fd, text_content, i);
+
+if (r == -1)
+return (-1);
+
+close(fd);
+return (1);
+}
+
