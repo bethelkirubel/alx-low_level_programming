d85693d1949b8fbf4d6416340e7626940f53f37a
Bethel Kirubel <bethelkirubel2127@gmail.com>
2023-09-04 11:43:11 -0700

    file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the file and writes the file and the number of letters
+ * @filename: the file
+ * @letters: the size of the file
+ * Return: the whole file and the number of letters or NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t nwr, nrd;
+char *buf;
+
+if (!filename)
+return (0);
+
+fd = open(filename, O_RDONLY);
+
+if (fd == -1)
+return (0);
+
+buf = malloc(sizeof(char) * (letters));
+
+if (!buf)
+return (0);
+
+nrd = read(fd, buf, letters);
+nwr = write(1, buf, nrd);
+
+close(fd);
+free(buf);
+
+return (nwr);
+}
+
